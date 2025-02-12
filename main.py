11096a872c1b3cac4daedc941b896c0b9252aa53
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-02-12 12:26:16 -0600

    Reworks the initial struture to use TabbedContent and Panes instead and lays out the skeleton for the content panes

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from datetime import datetime
 
 from textual.app import App, ComposeResult
-from textual.containers import Container
-from textual.widgets import Header, Footer, Static, Tabs, Tab
+from textual.containers import Container, Horizontal
+from textual.widgets import (Header, Footer,
+                             TabbedContent, TabPane, ListView,
+                             Static)
 
 
 class MainApp(App):
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Tabs(
-            Tab("Inventory", id="inventory"),
-            Tab("Recipes", id="recipes"),
-            Tab("Shopping Lists", id="shopping_lists")
-        )
+
+        with TabbedContent():
+            with TabPane("Inventory"):
+                self.inventory_list = ListView()
+                yield self.inventory_list
+            with TabPane("Recipe Book"):
+                with Horizontal():
+                    self.recipe_list = ListView()
+                    self.recipe_preview = Static(
+                        "This is the recipe preview pane", classes="content")
+                    yield self.recipe_list
+                    yield self.recipe_preview
+
+            with TabPane("Shopping Lists"):
+                with Horizontal():
+                    self.shopping_list = ListView()
+                    self.shopping_list_items = Static(
+                        "This is the items preview pane", classes="content")
+                    yield self.shopping_list
+                    yield self.shopping_list_items
+
         yield Footer()
 
 
