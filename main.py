643cff008f34aaa34f57561dde1edc2a5eb50d85
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-02-15 22:40:17 -0600

    Sorted navigation bar and basic inventory panel

--- main.py
+++ main.py
@@ -4,18 +4,41 @@ from textual.app import App, ComposeResult
 from textual.containers import Container, Horizontal
 from textual.widgets import (Header, Footer,
                              TabbedContent, TabPane, ListView,
-                             ListItem, Static, Button, Input)
+                             ListItem, Static, Button, Input, DataTable)
+
 
 class NavigationBar(Horizontal):
     """The main navigation bar that houses the Inventory, Recipe Book, and Shopping Lists tabs aligned to the left along with
     the Add, Edit, and Delete Buttons aligned to the right."""
-    def compose(self) -> ComposeResult:
-        yield Static()
 
-class InventoryPanel(Static):
-    """The panel that houses the Inventory content."""
     def compose(self) -> ComposeResult:
-        yield Static()
+        # Left-aligned tabs in a container
+        with Horizontal():
+            yield Button("Inventory", id="inventory_tab")
+            yield Button("Recipe Book", id="recipe_book_tab")
+            yield Button("Shopping Lists", id="shopping_lists_tab")
+
+        # Right-aligned buttons in a container
+        with Horizontal():
+            yield Button("Add", id="add_button")
+            yield Button("Edit", id="edit_button")
+            yield Button("Delete", id="delete_button")
+
+
+class InventoryPanel(DataTable):
+    """The panel that houses the Inventory content."""
+    inventory_data = [
+        ("Name", "Quantity", "Unit", "Expiration Date", "Category"),  # Headers
+        ("Apples", 1, "lbs", "", "Food"),
+        ("Rice", 4, "lbs", "", "Food"),
+        ("Chicken", 2, "lbs", "2025-03-20", "Food")
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield DataTable()
+        self.add_columns(*self.inventory_data[0])
+        self.add_rows(self.inventory_data[1:])
+
 
 class RecipeBookPanel(Static):
     """The panel that houses the Recipe Book content."""
@@ -69,6 +92,7 @@ class MainApp(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield NavigationBar()
+        yield InventoryPanel()
         yield Footer()
 
 
