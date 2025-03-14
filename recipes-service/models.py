c7e2666257455b8bcab5db42cbb5605c3fb050fd
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-03-13 20:28:44 -0600

    Yes

--- recipes-service/models.py
+++ recipes-service/models.py
@@ -2,7 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Recipe(db.Model):
+
+class Recipes(db.Model):
     __tablename__ = 'Recipes'
     Recipe_ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(100), nullable=False)
@@ -15,4 +16,15 @@ class Recipe(db.Model):
             'Name': self.Name,
             'Instructions': self.Instructions,
             'Description': self.Description
+        }
+
+class Ingredients(db.Model):
+    __tablename__ = 'Ingredients'
+    Item_ID = db.Column(db.Integer, db.ForeignKey('Items_In_House.Item_ID'), primary_key=True)
+    Recipe_ID = db.Column(db.Integer, db.ForeignKey('Recipes.Recipe_ID'), primary_key=True)
+
+    def to_dict(self):
+        return {
+            'Recipe_ID': self.Recipe_ID,
+            'Item_ID': self.Item_ID
         }
--- recipes-service/recipes_service.py
+++ recipes-service/recipes_service.py
@@ -1,9 +1,30 @@
+from types import new_class
+
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-from models import db, Recipe
+from models import db, Recipes, Ingredients
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://cs361_bakerc22:<password>@classmysql.engr.oregonstate.edu:3306/cs361_bakerc22'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.route('/get_recipes', methods=['GET'])
+def get_recipes():
+    recipes = Recipes.query.all()
+    return jsonify(recipes)
+
+@app.route('/add_recipe', methods=['POST'])
+def add_recipe(recipe_json):
+    new_recipe = Recipes(Name=recipe_json.Name, Instructions=recipe_json.Instructions, Description=recipe_json.Description)
+    db.session.add(new_recipe)
+    db.session.commit()
+
+@app.route('/delete_recipe', methods=['POST'])
+def delete_recipe(recipe_id):
+    recipe = Recipes.query.get(recipe_id)
+    db.session.delete(recipe)
+    db.session.commit()
+
+
 db.init_app(app)
